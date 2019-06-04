400f98aac6850ee4e5abef7aaf39501c19d975b4
Newgarden <jnewgarden92@gmail.com>
2019-06-04 11:24:15 -0500

    Header/Wrapper.js logic

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import "./style.css";
-
-function Wrapper(props) {
-  return <main className="wrapper" {...props} />;
-}
-
-export default Wrapper;
+export { default } from "./Wrapper";
